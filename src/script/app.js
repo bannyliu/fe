3d68a9dee095a0bd2c32be846cf46e372491be4f
412996304 <412996304@qq.com>
2017-05-17 23:03:12 +0800

    05172303fengfan

--- src/script/app.js
+++ src/script/app.js
@@ -8,20 +8,32 @@ import {Router, Route, hashHistory, browserHistory, IndexRoute, IndexRedirect} f
 import store from './redux/store'
 import { Provider } from 'react-redux'
 
+//index.js
 import Index from './components/Index'
+//index 主页面
+import IndexList from './components/index/indexList'
+//video 视频
 import VideoList from './components/video/List'
+//qanda 问答
 import QandaList from './components/qanda/qandaList'
 import QandaDetail from './components/qanda/qandaDetail'
 import QandaAdd from './components/qanda/qandaAdd'
-
+//job 招聘
 import JobList from './components/job/jobList'
 // import QandaDetail from './components/qanda/qandaDetail'
 
 ReactDOM.render((
   <Provider store={store().store}>
     <Router history={hashHistory}>
+
       <Route path="/" component={Index}>
-        <IndexRedirect to="/qanda/list" />
+        <IndexRedirect to="/index/list" />
+
+        {/* 主页面 index*/}
+        <Route path="index">
+          <Route path="list" component={IndexList}></Route>
+        </Route>
+
 
         {/* 视频  video*/}
         <Route path="video">
@@ -34,7 +46,7 @@ ReactDOM.render((
           <Route path="detail/:id" component={QandaDetail}></Route>
           <Route path="add" component={QandaAdd}></Route>
         </Route>
-        
+
         {/* 招聘 job*/}
         <Route path="job">
           <Route path="list" component={JobList}></Route>
