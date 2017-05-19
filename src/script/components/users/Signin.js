fa7c5804c4f240152bfc6693eae7d87db3290262
liguangyao88 <845972904@qq.com>
2017-05-19 09:18:04 +0800

    5.19_9:17_lgy

--- src/script/components/users/Signin.js
+++ src/script/components/users/Signin.js
@@ -2,7 +2,29 @@ import React, {Component} from 'react'
 
 import {Link} from "react-router"
 
+import Axios from "../../utils/axios.util.js"
+//console.log(Axios)//ok
+
 export default class Signin extends Component {
+	constructor(props) {
+    super(props)
+//  this.state = {
+//    username: localStorage.getItem('username')
+//  }
+  }
+	
+	login(){
+//		console.log(this)
+		let usersemail= this.refs.usersemail.value
+		let userspwd = this.refs.userspwd.value
+		let data = {
+			username:usersemail,
+			password:userspwd
+		}
+//		console.log(data)
+
+	}
+	
   render() {
     return (
       <div className="signin">
@@ -10,7 +32,7 @@ export default class Signin extends Component {
       	<span className="signin_xian"></span>
       	<input type="password" ref="userspwd" placeholder="请输入登录密码"/>
       	<span className="signin_xian"></span>
-      	<input type="button" className="signin_login" value="登录" />
+      	<input type="button" className="signin_login" onClick={this. login.bind(this)} value="登录" />
       	<b></b>
       	<Link to="/users/forgotpwd">忘记密码</Link>
       </div>
