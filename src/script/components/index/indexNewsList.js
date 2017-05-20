00251cec9a31ff417f96e65c4f94c2b3961e5a9e
412996304 <412996304@qq.com>
2017-05-20 13:07:21 +0800

    liubin0520

--- src/script/components/index/indexNewsList.js
+++ src/script/components/index/indexNewsList.js
@@ -1,6 +1,5 @@
 import React,{Component} from 'react'
 import IndexCommonTitle from './indexCommon'
-import IndexNewsContent from './indexNewsContent'
 import AxiosUtils from '../../utils/axios.js'
 
 class IndexNewsList extends Component{
@@ -10,21 +9,44 @@ class IndexNewsList extends Component{
       data:[{}]
     }
   }
+  indexNewsList(list){
+    return list.map((value,index)=>{
+      if(index < 5)
+      {
+        return(
+          <div className="news_content_list">
+          <p>{value.title}</p>
+          <p className="news_content_time">{value.createDate}</p>
+          <br />
+          </div>
+        )
+      }
+    })
+  }
   render(){
     return(
       <li className="news_list">
         <IndexCommonTitle title={this.props.title}/>
-        <IndexNewsContent />
+        <div className="news_content">
+          {this.indexNewsList(this.state.data)}
+        </div>
       </li>
     )
   }
   componentDidMount(){
+    let tabs = this.props.tabs
+    let that = this
     AxiosUtils.get({
-      url:'api/qanda/list',
+      url: `api/${tabs}/list`,
       method:'get',
-      data:{},
+      data:{
+        start:0,
+        count:10
+      },
       callback:function(res){
-        console.log(res.data.data.subjects)
+        that.setState({
+          data:res.data.data.subjects
+        })
       }
     })
   }
